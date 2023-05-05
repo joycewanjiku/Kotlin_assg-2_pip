0156536a22ebc89f0c6ecf33264e643c50fb1134
student <joycemumbiiwanjiku@gmail.com>
2023-05-05 23:57:52 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main() {
+println(ditails())
+    println( structure())
+}
+fun ditails(){
+    var ditails="My name is Joyce."+" I love playing"
+
+}
+
+//1. Write a function that takes in 3 parameters, name, age, and country,
+// and returns a String with this structure “Hi, my name is x,
+// I am y years old and I am from z.” Where x, y, and z are the provided name, age,
+// and country respectively. (3 points)
+fun structure(  name:String,age:Int connte){
+     var details="Hi,"+"my name is joy"+" I am 43 years old"+" I am from Kenya"
+}
+//2. Write a function that takes in a String and returns its length (2 points)
+fun strings(words:String): Int{
+    return ("the length  words String")
+
+}
+//3. Write a function that takes in a name and prints out
+// “That’s me!” when your name is passed to it, otherwise, it prints out
+// “I don’t know who that is” (3 points)
+fun names( names:String):String{
+    if (names==names){
+        return "thats me"
+    }
+    else{
+        return  "i dont know who that is"
+    }
+
+}
+
+//4. Write a Kotlin function that takes in a string will all the vowels
+// replaces by the character ‘*’. Use string interpolation to generate the output. (2 points)
